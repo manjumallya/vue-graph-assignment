6d8316c9e81dbe6ced476781dcc95c57489bad0e
Konaje <konaje.manjunath@capgemini.com>
2024-02-06 10:48:17 +0100

    express server script setup

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -2,21 +2,21 @@
 
 describe('App.vue', () => {
   it('visits the app root url and checks if the nodes are loaded', () => {
-    cy.visit('http://localhost:3000');
+    cy.visit('http://localhost:8080');
     cy.contains('Fetch Nodes').click();
     cy.get('[data-cy=parent-nodes]').first().click();
     cy.get('[data-cy=nodes-data]').should('be.visible');
   })
 
   it('Checks if the popup is opened on click of the node', () => {
-    cy.visit('http://localhost:3000');
+    cy.visit('http://localhost:8080');
     cy.contains('Fetch Nodes').click();
     cy.get('[data-cy=parent-nodes]').first().click();
     cy.get('[data-cy=sidebar-popup]').should('be.visible');
   })
 
   it('Close the sidebar popup after opening', () => {
-    cy.visit('http://localhost:3000');
+    cy.visit('http://localhost:8080');
     cy.contains('Fetch Nodes').click();
     cy.get('[data-cy=parent-nodes]').first().click();
     cy.get('[data-cy=close-button]').first().click();
